d337285923a664d410a88ebb28137be78566a931
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-08 13:36:54 -0300

    add morgan, urlencoded, json express middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const logger = require('morgan')
 const app = express();
+
+app.use(express.urlencoded({ extended : false }));
+app.use(express.json());
+app.use(logger('dev'));
 
 app.use('/messages', require('./controllers/message'));
 
