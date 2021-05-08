8622837220277f209e28b31b1faf992562028c3e
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-08 15:43:23 -0300

    add public folder and add static files setting

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const logger = require('morgan')
+const logger = require('morgan');
+const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended : false }));
 app.use(express.json());
 app.use(logger('dev'));
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.use('/examples', require('./controllers/example'))
 app.use('/messages', require('./controllers/message'));
