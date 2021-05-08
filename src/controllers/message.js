dd5149cc22c5ad2377a48357081985d34f9c18e2
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-08 13:44:43 -0300

    prueba req.body

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => res.send('Message list'));
-router.post('/', (req, res) => res.send('Message created'));
+router.get('/', (req, res) => {
+  res.send('Message list');
+});
+
+router.post('/', (req, res) => {
+  // Body
+  console.log(req.body);
+  const { message, name } = req.body;
+  console.log(message, name);
+
+  res.send('Message created')
+});
 
 module.exports = router;
