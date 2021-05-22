d606ffd90bf2e4d5bc3014636c843a772da1b0ad
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-22 01:03:17 -0300

    connection bd added and change persis method

--- src/components/message/store.js
+++ src/components/message/store.js
@@ -1,7 +1,22 @@
-let list = [];
+const db = require('mongoose');
+const Model = require('./model')
 
-const persist = (message) => {
-  list.push(message);
+// db.Promise = global.Promise
+db.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+  .then((db) => console.log("DB is CONNECTED"))
+  .catch((err) => console.error(err));
+console.log('[db] Connected')
+// let list = [];
+
+const persist = async (message) => {
+  // list.push(message);
+  const newMessage = new Model(message);
+  const aMessage = await newMessage.save();
+  return aMessage;
 };
 
 const getAll = () => {
