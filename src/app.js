0d3e065bb87d8855ee117b837a4115ad454ff9f2
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-25 23:44:30 -0300

    socket chat express

--- src/app.js
+++ src/app.js
@@ -2,34 +2,33 @@ const express = require('express');
 const app = express();
 const logger = require('morgan');
 const path = require('path');
+const server = require('http').Server(app);
+const socket = require('./socket');
+
 const db = require('./config/db');
-// const routerExample = require('./components/example/network');
-// const routerMessage = require('./components/message/network');
+
 const router = require('./networks/routes');
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+app.use(express.static('public'));
 
-app.use(express.static('public'))
+// io.on('Connection', (socket) => {
+//   console.log('nuevo cliente conectado');
+//   socket.emit('mensaje', 'Bienvenido!');
+// })
 
-io.on('Connection', (socket) => {
-  console.log('nuevo cliente conectado');
-  socket.emit('mensaje', 'Bienvenido!');
-})
+// setInterval(() => {
+//   io.emit('mensaje', 'Hello all')
+// }, 3000);
 
-setInterval(() => {
-  io.emit('mensaje', 'Hello all')
-}, 3000);
-
-db(process.env.MONGODB_URI)
+db(process.env.MONGODB_URI);
 
 app.use(express.urlencoded({ extended : false }));
 app.use(express.json());
 app.use(logger('dev'));
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
-// app.use(routerExample);
-// app.use(routerMessage);
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+socket.connect(server);
 router(app);
 
 module.exports = server;
--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config();
 }
 
-const app = require('./app');
+const server = require('./app');
 
 const port = 5000;
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+server.listen(port, () => console.log(`Server listening on port ${port}!`));
