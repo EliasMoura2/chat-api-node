431fda53d2f628748831d43f9561dbe5ecc70361
Elias Moura <d.eliasmoura2@gmail.com>
2021-05-25 23:54:50 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,32 +1,25 @@
 const express = require('express');
-const app = express();
 const logger = require('morgan');
 const path = require('path');
+const app = express();
 const server = require('http').Server(app);
 const socket = require('./socket');
+const cors = require('cors');
 
 const db = require('./config/db');
 
 const router = require('./networks/routes');
 
-app.use(express.static('public'));
-
-// io.on('Connection', (socket) => {
-//   console.log('nuevo cliente conectado');
-//   socket.emit('mensaje', 'Bienvenido!');
-// })
-
-// setInterval(() => {
-//   io.emit('mensaje', 'Hello all')
-// }, 3000);
 
 db(process.env.MONGODB_URI);
 
+app.use(cors())
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended : false }));
 app.use(express.json());
-app.use(logger('dev'));
-
 app.use('/static', express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
+
 
 socket.connect(server);
 router(app);
